2cb1e96020d05e09e7a984032ee694ccdcb1db31
Blake Frederick <bf@six12creative.com>
2024-03-03 11:59:10 -0800

    `unboldPreexistingText`

--- content.js
+++ content.js
@@ -17,6 +17,14 @@ document.addEventListener('keydown', function(event) {
     }
 })
 
+// First remove bold from preexisting text
+function unboldPreexistingText(element) {
+    const textElements = element.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, a, li, td, th, b, strong, .bold')
+    textElements.forEach(el => {
+        el.style.fontWeight = 'normal'
+    })
+}
+
 function toProcessWord(word) {
     const cutoff = Math.ceil(word.length * 0.6) // Bold the first 60% of the word
     return [word.slice(0, cutoff), word.slice(cutoff)]
@@ -24,7 +32,7 @@ function toProcessWord(word) {
 
 // Process text node and apply br bold and fade classes
 function processTextNode(node) {
-    // no svgs, imgs
+    // filter out some non-text tags
     if (!node.parentNode.closest('svg, img, canvas, style')) {
         const words = node.textContent.trim().split(/\s+/)
         const processedWords = words.map(word => {
@@ -59,6 +67,7 @@ style.textContent = `
   }
 `
 document.head.appendChild(style)
+unboldPreexistingText(document.body)
 document.body.style.setProperty('--fade-level', fadeLevels[currentFadeIndex])
 applyBoldReadingToElement(document.body)
 document.body.classList.add('br')
